d52bbe889421ac60855c900b1a7d0da700be8421
DiegoB <diegobaesv@gmail.com>
2025-01-07 22:30:34 -0500

    cambios evt usuariologin

--- src/app.js
+++ src/app.js
@@ -11,6 +11,16 @@ app.use(express.static('public'));
 io.on('connection', async (socket)=>{
     const { id, handshake } = socket;
     console.log(`Se ha conectado [${id}] [${handshake.address}] - ${handshake.time}`);
+
+    //RECIBO EVENTO LOGIN ENVIADO DESDE EL USUARIO X
+    socket.on('evt_usuarioLogin',async (usuario)=>{
+        console.log('Se ha logueado el usuario', usuario);
+        //AVISO A LOS DEMAS USUARIOS QUE EL USUARIO X SE HA CONECTADO
+        io.emit('evt_usuarioLogin',{
+            ...usuario, 
+            time: handshake.time
+        });
+    });
 });
 
 module.exports = httpServer; 
